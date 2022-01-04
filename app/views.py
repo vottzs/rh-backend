1f891ee54ceb9cf9e28d03e4157e6e41a1649aa4
vottzs <97006916+vottzs@users.noreply.github.com>
2022-01-03 19:59:49 -0800

    Update and rename app/views/__init__.py to app/views.py

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+def add_views(app: Flask):
+    app.add_url_rule('/hiring_stages', view_func=get_hiring_stages)
+
+def get_hiring_stages():
+    response_object = {'status': 'success'}
+    response_object['hiring_stages'] = HIRING_STAGES
+    response = jsonify(response_object)
+    return response
