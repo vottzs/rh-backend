64924c6134d6b10eaf71ebdb6e78a6f58440753f
meirycooper <meiry_2006@hotmail.com>
2022-01-10 13:43:00 -0300

    Inclusion of Job application data

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -1,5 +1,6 @@
 HIRING_STAGES = [
   'Applied',
+  'Resume Analysis',
   'Contacted',
   'Interview',
   'Interview Done',
@@ -8,4 +9,30 @@ HIRING_STAGES = [
   'Offer Accepted',
   'Offer Declined',
   'Hired'
+]
+
+JOB_STATUS = [
+  'Private',
+  'Published',
+  'Accepting Applications',
+  'Interviewing Candidates',
+  'Accepting Candidates'
+]
+
+JOB_BENEFITS = [
+  'Vacations',
+  'Life Insurance',
+  'Health Insurance',
+  'Dental Insurance',
+  'Parental Leaving',
+  'Retirement Accounts'
+]
+
+HIRING_TYPES = [
+  'Contract',
+  'Full time',
+  'Part time',
+  'Freelancer',
+  'Intership',
+  'Temporary'
 ]
