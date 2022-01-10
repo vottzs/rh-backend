a6d450b13ca0f4b143986c0ffa758eece660ff4b
pabloraribeiro <halamano@gmail.com>
2022-01-10 13:42:58 -0300

    Inclusion of Job benefits, Hiring types and Hiring stages

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -9,3 +9,36 @@ HIRING_STAGES = [
   'Offer Declined',
   'Hired'
 ]
+JOB_STATUS = [
+'Private'
+'Published'
+'Accepting Candidates'
+'Closed'
+]
+JOB_BENEFITS = [
+  'Vacation'
+  'Life Insurance'
+  'Health Insurance'
+  'Disability Insurance'
+  'Dental and Vision Insurance'
+  'PTO/Paid Holidays'
+  'Retirement Planning'
+  'Family Leave'
+  'Remote work'
+  'Flex Schedule'
+]
+HIRING_TYPES = [
+  'Contract'
+  'Full Time'
+  'Part Time'
+  'Free Lancer'
+  'Temporary'
+  'Internship'
+]
+HIRING_STAGES = [
+  'Add Resume'
+  'Analisys'
+  'Approved'
+  'Regected'
+  'Stand By'
+]
