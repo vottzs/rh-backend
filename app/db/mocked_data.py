dabb25a1dc8a1c2dc1a260c260d6fd3db65d7c9e
isadcoutinho <isadcoutinho@gmail.com>
2022-01-11 20:01:49 -0300

    Application Scientist

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -9,3 +9,26 @@ HIRING_STAGES = [
   'Offer Declined',
   'Hired'
 ]
+HIRING_TYPES = [
+  'Contract',
+  'Full time',
+  'Part time',
+  'Remote',
+  'Presencial'
+]
+JOB_BENEFITS = [
+  'Vacation',
+  'Life Insurance',
+  'Healthcare Insurance',
+  'Parental Living',
+  'Food vouchers'
+  'Gift vouchers',
+  'Sick pay',
+  'Discounts across travel'
+]
+JOB_STATUS = [
+  'Private',
+  'Publishing',
+  'Accepting candidates',
+  'Close date'
+]
