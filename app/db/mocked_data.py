00c15a932d8a39f41972d4f3c46bb1edb0973e13
Igor Barcellos <igorbarcellosdrums@gmail.com>
2022-01-10 21:01:45 -0300

    I refreshed HIRING STAGES content and created new constants.

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -1,5 +1,6 @@
 HIRING_STAGES = [
   'Applied',
+  'Resume Analysis'
   'Contacted',
   'Interview',
   'Interview Done',
@@ -9,3 +10,24 @@ HIRING_STAGES = [
   'Offer Declined',
   'Hired'
 ]
+HIRING_TYPES = [
+  'Contract'
+  'Full Time'
+  'Part Time'
+  'Promotion'
+]
+JOB_STATUS = [
+  'Private'
+  'Published'
+  'Accepting Candidates'
+  'Reviewing Applications'
+
+]
+JOB_BENEFITS = [
+  'Vacations'
+  'Life Insurance'
+  'Parental Leaving'
+  'Paid Holidays'
+  'Dental Insurance'
+]
+
