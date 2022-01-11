74ab7ce3104fe481d296e457e69b86fc42622001
Leonardo Lobo <gutierres324@gmail.com>
2022-01-10 21:03:34 -0300

    PROJECT UPDATE

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -1,5 +1,6 @@
 HIRING_STAGES = [
   'Applied',
+  'Resume Analysis'
   'Contacted',
   'Interview',
   'Interview Done',
@@ -8,4 +9,48 @@ HIRING_STAGES = [
   'Offer Accepted',
   'Offer Declined',
   'Hired'
+]
+HIRING_TYPES = [
+  'Contract',
+  'Full Time',
+  'Part Time',
+  'Free Lancer',
+  'Temporary',
+  'Internship'
+]
+JOB_STATUS = [
+  'Private',
+  'Published',
+  'Accepting Cadidates',
+  'Closed'
+]
+JOB_BENEFITS = [
+  'Vacation',
+  'Life Insurance',
+  'Health Insurance',
+  'Disability Insurance',
+  'Dental and Vision Insurance',
+  'PTO/Paid Holidays',
+  'Retirement Planning',
+  'Family Leave',
+  'Remote work',
+  'Flex Schedule'
+]
+CADIDATES = [
+  'Ada Lovelace',
+  'Grace hopper',
+  'Margaret Hamilton',
+  'Joan Clarke'
+]
+JOB_POSTINGS = [
+
+]
+OFFICES = [
+  
+]
+COMPANIES = [
+  
+]
+USERS = [
+  
 ]
