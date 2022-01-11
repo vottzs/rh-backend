132da569946c6331453e1240da68206961957c67
pabloraribeiro <halamano@gmail.com>
2022-01-11 14:47:16 -0300

    Added Candidates, Offices and Job posting to backend

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -31,7 +31,7 @@ HIRING_TYPES = [
   'Contract'
   'Full Time'
   'Part Time'
-  'Free Lancer'
+  'Freelancer'
   'Temporary'
   'Internship'
 ]
@@ -41,4 +41,59 @@ HIRING_STAGES = [
   'Approved'
   'Regected'
   'Stand By'
+]
+
+CANDIDATES = [
+    {
+	‘name’: ‘Zelda Ribeiro’,
+	‘address’: ‘Rua Manoel Conceição, 1034, Vila Rezende, Piracicaba, Brasil’,
+	‘phone’: ‘+5519987654321’,
+	‘linkedin’: ‘www.linkedin.com/zeldar’
+    },
+    {‘name’: ‘Daisy Cooper’,
+	‘address’: ‘Avenida Dona Francisca, 643, Vila Rezende, Piracicaba, Brasil’,
+	‘phone’: ‘+5519998765432’,
+	‘linkedin’: ‘www.linkedin.com/daisyc’
+    },
+    {‘name’: ‘Princesa Jujuba’,
+	‘address’: ‘Avenida Lourenço Ducatti, 835 , Vila Rezende, Piracicaba, Brasil’,
+	‘phone’: ‘+551999876543’,
+	‘linkedin’: ‘www.linkedin.com/pjujuba’
+    }
+]
+
+OFFICES = [
+    {
+	‘name’: ‘Escritório Matriz’,
+	‘address’: ‘Rua Manoel Conceição, 1034, Vila Rezende, Piracicaba, Brasil’,
+	‘phone’: ‘+5519987654321’,
+    },
+    {‘name’: ‘Escritório Filial I’,
+	‘address’: ‘Avenida Dona Francisca, 643, Vila Rezende, Piracicaba, Brasil’,
+	‘phone’: ‘+5519998765432’,
+    },
+    {‘name’: ‘Escritório Filial II’,
+	‘address’: ‘Avenida Lourenço Ducatti, 835 , Vila Rezende, Piracicaba, Brasil’,
+	‘phone’: ‘+551999876543’,
+    }
+]
+JOB_POSTING = [
+    {      
+	‘Title’: ‘Boss’,
+	‘Description’: ‘One job to rule them all’,
+	‘Salary Range Min’: ‘$5000’,
+	‘Salary Range Max’: ‘$6000’
+    },
+    {
+    ‘Title’: ‘Psychologist’,
+	‘Description’: ‘Take care of the employee mental health’,
+	‘Salary Range Min’: ‘$3000’,
+	‘Salary Range Max’: ‘$3200’
+    },
+    {
+    ‘Title’: ‘Gardener’,
+	‘Description’: ‘Aguar as plantas’,
+	‘Salary Range Min’: ‘$2000’,
+	‘Salary Range Max’: ‘$2100’
+    },
 ]
