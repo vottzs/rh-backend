9022e4ef2b4ac20f230b54300d91f8ab2753efe4
MAnoel <manoelforum@gmail.com>
2022-01-11 20:55:16 -0300

    new auterations

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -14,16 +14,75 @@ JOB_STATUS = [
   'Private',
   'Published',
   'Accepting Candidates'
+  'Close Date',
+  'Seasonal'
 ]
 
 JOB_BENEFITS= [
   'Vacations',
   'Life Insurance',
-  'Parental Leaving'
+  'Parental Leaving',
+  'Health Insurance Benefits',
+  'Life Insurance',
+  'Dental Insurance',
+  'Retirement Accounts',
+  'Paid Vacation',
+  'Paid Medical Leave',
+  'Flexible Schedules',
+  'Education Assistance'
+
 ]
 
 HIRING_TYPES=[
   'Contract',
   'Full time',
-  'Part time'
+  'Part time',
+  'Seasonal',
+  'Temporary'
+]
+
+CANDIDATES=[
+  { 'name':'Harry Potter',
+    'adress':'Rua dos Pássaros, 94, Lagoa nova, Brasil',
+    'phone':'+58485485485',
+    'linkedin':'www.linkedin.com/harrypotter'
+    }
+
+  { 'name':'Joaquim da rua',
+    'adress':'Avenida Prudente de Morais, 58, Candelária, Brasil',
+    'phone':'+584456275433',
+    'linkedin':'www.linkedin.com/Joaquimdarua'
+    }
+  { 'name':'Joaquina da avenida',
+    'adress':'Rua sem saída, 33, Bora Bora, Brasil',
+    'phone':'+58495132425',
+    'linkedin':'www.linkedin.com/Joaquinadaavenida'
+    }
+
+]
+
+OFFICES_DATA=[
+  {'name':'Tom Cruise',
+   'adress':'Rua Dom Pedro II, 85, Boa Vista, Brasil',
+   'phone':'+8521458742145'
+   }
+
+  {'name':'Angelina Jolie',
+   'adress':'Rua das flores, 84, Corumbá, Brasil',
+   'phone':'+859636965487'
+   }
+
+  {'name':'Brad Pitt',
+   'adress':'Santa Maria, 83, Centro, Brasil',
+   'phone':'+852584714747'
+   }
+]   
+  JOB_POSTING_DATA=[
+    
+  { 'Title':'Childrens Party Animator',
+    'Description':'Do your best to transform Childrens Day',
+    'Salary Range Min':'$ 500,00'
+    'Salary Range Max':'$ 2000,00' 
+    }
+
 ]
