418f03f644e58ee162c3a2b1ba458d0aa3b98034
Igor Barcellos <igorbarcellosdrums@gmail.com>
2022-01-11 20:28:07 -0300

    I added new diccionaries (CANDIDATES, OFFICES and JOB_POSTINGS)

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -30,4 +30,42 @@ JOB_BENEFITS = [
   'Paid Holidays'
   'Dental Insurance'
 ]
-
+CANDIDATES =[
+  {'name': 'Larissa Lopes',
+  'address': 'Rua Tamboriu, 400, Pq. Uirapuru, Guarulhos'
+  'phone'; '321654987'
+  'linkedin': 'www.linkedin.com/larissalopes'},
+  {'name': 'Draco Malfoy',
+  'address': 'Wiltshire, England'
+  'phone'; '79845613'
+  'linkedin': 'www.linkedin.com/sujeitinhadesangueruim'},
+  {'name': 'Ragnar Lothbrok',
+  'address': 'Kategat, Norway'
+  'phone'; 'ᛋᛒᛟᛚᚨ'
+  'linkedin': 'www.linkedin.com/ragnarlothbrok'}
+]
+OFFICES = [
+  {'name': 'Genion Tecnologies',
+  'address': 'Rua Cardeal Arcoverde, Pinheiros, SP',
+  'phone': '11937101888'},
+  {'name': 'Promoluck',
+  'address': 'R. Petrobrás, 292, Vila Antonieta, SP',
+  'phone': '1123612191'},
+  {'name': 'Barcellos Frazão Comércio Digital',
+  'address': 'Rua São Domingos, 228, Guarulhos, SP',
+  'phone': '11930991876'},
+]
+JOB_POSTINGS = [
+  {'title': 'Help Desk Analyst',
+  'description': 'The Help Desk Analyst\'s role is to ensure proper computer operation so that end users can accomplish business tasks. This includes receiving, prioritizing, documenting, and actively resolving end user help requests.',
+  'salary range min': '$1500,00',
+  'salary range max': '$2500,00'},
+  {'title': 'Product Designer',
+  'description': 'the product designer\'s role is to develop new products and design molds for plastic injection. ',
+  'salary range min': '$100,00',
+  'salary range max': '$3000,00'},
+  {'title': 'Customer Service',
+  'description': 'The costumer service professional must be up to give our costumers support in all their needs about post buying.',
+  'salary range min': '$2000,00',
+  'salary range max': '$4000,00'}
+]
