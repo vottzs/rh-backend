a3595a3cc518f7b6e05d3e228cc9f2c130086fc6
Leonardo Lobo <gutierres324@gmail.com>
2022-01-11 21:01:56 -0300

    CANDIDATES, JOB POSTINGS, OFFICES UPDATE

--- app/db/mocked_data.py
+++ app/db/mocked_data.py
@@ -36,17 +36,53 @@ JOB_BENEFITS = [
   'Remote work',
   'Flex Schedule'
 ]
-CADIDATES = [
-  'Ada Lovelace',
-  'Grace hopper',
-  'Margaret Hamilton',
-  'Joan Clarke'
+CANDIDATES = [
+  {'name': 'Regan Montgomery',
+  'adress': '684-8768 Mollis Avenue, CA, US',
+  'phone:': '1-825-765-8760',
+  'linkedin': 'www.linkedin.com/ReganMontgomery'
+  },
+  {'name': 'Brandon Green',
+  'adress': 'P.O. Box 994, 6983 Aliquam Rd., CA, US',
+  'phone:': '1-271-439-7367',
+  'linkedin': 'www.linkedin.com/BrandonGreen'
+  },
+  {'name': 'Sonia Hartman',
+  'adress': '867-2599 Non, Road, CA, US',
+  'phone:': '(813) 717-3988',
+  'linkedin': 'www.linkedin.com/SoniaHartman'
+  }
 ]
 JOB_POSTINGS = [
-
+{'title': 'Medical Assistant',
+'description': 'To provide assitance to the doctors during any procedure',
+'salary range min': '$2000,00',
+'salary range max': '$2500,00'
+},
+{'title': 'Substitute Teacher',
+'description': 'To ministrate classes to kids',
+'salary range min': '$3000,00',
+'salary range max': '$3500,00'
+},
+{'title': 'Zamboni Operator',
+'description': 'To operate the Zamboni to get fields ready',
+'salary range min': '$5000,00',
+'salary range max': '$9000,00'
+}
 ]
 OFFICES = [
-  
+  {'name': 'Burton Snider CO',
+'adress': 'Ap #496-3616 Magna. St., Jönköpings län, South Korea',
+'phone': '1-187-314-5479'
+},
+{'name': 'Philip Lambert C.O.',
+'adress': 'Ap #670-1991 Quis Rd., Lazio, Austria',
+'phone': '(418) 573-1929'
+},
+{'name': 'Ray Mitchell CO',
+'adress': 'P.O. Box 557, 6067 Eu, Road, Zuid Holland, New Zealand',
+'phone': '(438) 182-6683'
+}
 ]
 COMPANIES = [
   
