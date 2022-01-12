f9e43135a16fe6899232fd312203f964aa2bbcc6
Leonardo Lobo <gutierres324@gmail.com>
2022-01-12 20:28:34 -0300

    updates day 3

--- app/views.py
+++ app/views.py
@@ -1,11 +1,27 @@
 from flask import Flask, jsonify
 from app.db.mocked_data import DEFAULT_HIRING_STAGES
+from app.db.mocked_data import CANDIDATES
+from app.db.mocked_data import JOB_POSTINGS
 
 def add_views(app: Flask):
-    app.add_url_rule('/hiring_stages', view_func=get_hiring_stages)
+    app.add_url_rule('/api/v1/hiring_stages', view_func=get_hiring_stages)
+    app.add_url_rule('/api/v1/candidates', view_func=get_candidates)
+    app.add_url_rule('/api/v1/job_postings', view_func=get_job_postings)
 
 def get_hiring_stages():
     response_object = {'status': 'success'}
-    response_object['hiring_stages'] = DEFAULT_HIRING_STAGES
+    response_object['api/v1/hiring_stages'] = DEFAULT_HIRING_STAGES
+    response = jsonify(response_object)
+    return response
+
+    def get_candidates():
+    response_object = {'status': 'success'}
+    response_object['api/v1/candidates'] = CANDIDATES
+    response = jsonify(response_object)
+    return response
+
+    def get_job_postings():
+    response_object = {'status': 'success'}
+    response_object['api/v1/job_postings'] = JOB_POSTINGS
     response = jsonify(response_object)
     return response
