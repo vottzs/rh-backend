fa7f64d2ff2209694c2231ffb9801792180fe135
meirycooper <meiry_2006@hotmail.com>
2022-01-12 13:22:23 -0300

    Backend import Hiring Stages, Job Posting and Candidates

--- app/views.py
+++ app/views.py
@@ -2,10 +2,35 @@ from flask import Flask, jsonify
 from app.db.mocked_data import DEFAULT_HIRING_STAGES
 
 def add_views(app: Flask):
-    app.add_url_rule('/hiring_stages', view_func=get_hiring_stages)
+    app.add_url_rule('api/v1/hiring_stages', view_func=get_hiring_stages)
 
 def get_hiring_stages():
     response_object = {'status': 'success'}
     response_object['hiring_stages'] = DEFAULT_HIRING_STAGES
+    response = jsonify(response_object)
+    return response
+
+from flask import Flask, jsonify
+from app.db.mocked_data import CANDIDATES
+
+def add_views(app: Flask):
+    app.add_url_rule('api/v1/candidates', view_func=get_candidates)
+
+def get_candidates():
+    response_object = {'status': 'success'}
+    response_object['candidates'] = CANDIDATES
+    response = jsonify(response_object)
+    return response
+
+
+from flask import Flask, jsonify
+from app.db.mocked_data import JOB_POSTINGS
+
+def add_views(app: Flask):
+    app.add_url_rule('api/v1/job_postings', view_func=get_job_postings)
+
+def get_job_postings():
+    response_object = {'status': 'success'}
+    response_object['job_postings'] = JOB_POSTINGS
     response = jsonify(response_object)
     return response
