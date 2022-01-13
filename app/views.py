f8a91c2c9cb75a674055113b84faf158251ee885
DanielT <daniel@lungfu.com.br>
2022-01-13 12:37:17 -0300

    new routes again

--- app/views.py
+++ app/views.py
@@ -1,11 +1,30 @@
 from flask import Flask, jsonify
-from app.db.mocked_data import DEFAULT_HIRING_STAGES
+from app.models import HIRING_STAGES
 
 def add_views(app: Flask):
     app.add_url_rule('/hiring_stages', view_func=get_hiring_stages)
 
 def get_hiring_stages():
     response_object = {'status': 'success'}
-    response_object['hiring_stages'] = DEFAULT_HIRING_STAGES
+    response_object['hiring_stages'] = HIRING_STAGES
     response = jsonify(response_object)
     return response
+
+    def add_views(app: Flask):
+    app.add_url_rule('/candidates', view_func=get_candidates)
+
+    def get_candidates():
+    response_object = {'status': 'success'}
+    response_object['candidates'] = CANDIDATES
+    response = jsonify(response_object)
+    return response
+
+    def add_views(app: Flask):
+    app.add_url_rule('/job_posting', view_func=get_job_posting)
+
+    def get_candidates():
+    response_object = {'status': 'success'}
+    response_object['job_posting'] = JOB_POSTING
+    response = jsonify(response_object)
+    return response
+
