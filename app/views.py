6fdfce5ec397888c44daa4f19e91ac5ae8d84143
Igor Barcellos <igorbarcellosdrums@gmail.com>
2022-01-12 21:15:06 -0300

    Updated views.py creating more endpoints

--- app/views.py
+++ app/views.py
@@ -1,11 +1,59 @@
 from flask import Flask, jsonify
 from app.db.mocked_data import DEFAULT_HIRING_STAGES
+from app.db.mockes_data import JOB_BENEFITS_OPTIONS
+from app.db.mocked_data import HIRING_TYPES
+from app.db.mockes_data import HIRING_STAGES
+from app.db.mocked_data import CANDIDATES
+from app.db.mockes_data import OFFICES
+from app.db.mockes_data import JOB_POSTINGS
 
 def add_views(app: Flask):
-    app.add_url_rule('/hiring_stages', view_func=get_hiring_stages)
+    app.add_url_rule('/api/v1/default_hiring_stages', view_func=get_default_hiring_stages)
+    app.add_url_rule('/api/v1/job_benefits_options', view_func=get_job_benefits_options)
+    app.add_url_rule('/api/v1/hiring_types', view_func=get_hiring_types)
+    app.add_url_rule('/api/v1/hiring_stages', view_func=get_hiring_stages)
+    app.add_url_rule('/api/v1/candidates', view_func=get_candidates)
+    app.add_url_rule('/api/v1/get_offices', view_func=get_offices)
+    app.add_url_rule('/api/v1/job_postings', view_func=get_job_postings)
+
+def get_default_hiring_stages():
+    response_object = {'status': 'success'}
+    response_object['default_hiring_stages'] = DEFAULT_HIRING_STAGES
+    response = jsonify(response_object)
+    return response
+
+def get_job_benefits_options():
+    response_object = {'status': 'success'}
+    response_object['job_benefits_options']
+    response = jsonfy(response_object)
+    return response
+
+def get_hiring_types():
+    response_object = {'status': 'success'}
+    response_object['hiring_types']
+    response = jsonfy(response_object)
+    return response
 
 def get_hiring_stages():
     response_object = {'status': 'success'}
-    response_object['hiring_stages'] = DEFAULT_HIRING_STAGES
-    response = jsonify(response_object)
+    response_object['hiring_stages']
+    response = jsonfy(response_object)
+    return response
+
+def get_candidates():
+    response_object = {'status': 'success'}
+    response_object['candidates']
+    response = jsonfy(response_object)
+    return response
+
+def get_offices():
+    response_object = {'status': 'success'}
+    response_object['offices']
+    response = jsonfy(response_object)
+    return response
+
+def get_job_postings():
+    response_object = {'status': 'success'}
+    response_object['job_postings']
+    response = jsonfy(response_object)
     return response
