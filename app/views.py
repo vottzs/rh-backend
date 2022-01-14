64dbf9c58637ca8e150f6fb0c7684813c672398c
MAnoel <manoelforum@gmail.com>
2022-01-13 23:53:54 -0300

    Training

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
+from re import search
 from flask import Flask, jsonify
 from app.db.mocked_data import DEFAULT_HIRING_STAGES
 from app.db.mocked_data import JOB_BENEFITS_OPTIONS
 from app.db.mocked_data import HIRING_TYPES
-from app.db.mocked_data import HIRING_STAGES
 from app.db.mocked_data import CANDIDATES
 from app.db.mocked_data import OFFICES
 from app.db.mocked_data import JOB_POSTINGS
@@ -11,10 +11,10 @@ def add_views(app: Flask):
     app.add_url_rule('/api/v1/hiring_stages', view_func=get_hiring_stages)
     app.add_url_rule('/api/v1/job_benefits_options', view_func=job_benefits_options)
     app.add_url_rule('/api/v1/hiring_types', view_func=hiring_types)
-    app.add_url_rule('/api/v1/hiring_stages', view_func=hiring_stages)
     app.add_url_rule('/api/v1/candidates', view_func=candidates)
     app.add_url_rule('/api/v1/offices', view_func=offices)
     app.add_url_rule('/api/v1/job_postings', view_func=job_postings)
+    app.add_url_rule('/api/v1/candidates/<candidate_id>', view_func=get_candidate)
 
 def get_hiring_stages():
     response_object = {'status': 'success'}
@@ -31,12 +31,6 @@ def job_benefits_options():
 def hiring_types():
     response_object = {'status':'success'}
     response_object['hiring_types'] = HIRING_TYPES
-    response = jsonify(response_object)
-    return response
-
-def hiring_stages():
-    response_object = {'status':'success'}
-    response_object['hiring_Stages'] = HIRING_STAGES
     response = jsonify(response_object)
     return response
 
@@ -57,3 +51,12 @@ def job_postings():
     response_object['job_postings'] = JOB_POSTINGS
     response = jsonify(response_object)
     return response
+
+def get_candidate(candidate_id):
+    response_object = {'status':'success'}
+    response_object['candidate'] = search_on_list(CANDIDATES, 'id', int(candidate_id))
+    response = jsonify(response_object) 
+    return response
+
+def search_on_list(list_of_dictes, key, value):
+    return next(item for item in list_of_dictes if item.get(key) == value)
