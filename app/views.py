357c1708e869a14731b3dc72fba7ae3f8978512f
MsDyaneAlbuquerque <dyane.batory13.dca@gmail.com>
2022-01-17 01:39:53 -0300

    Retirada do Repo e alterando caminho da URL da api

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from app.db.mocked_data import DEFAULT_HIRING_STAGES
 
 def add_views(app: Flask):
-    app.add_url_rule('/hiring_stages', view_func=get_hiring_stages)
+    app.add_url_rule('/api/v1/hiring_stages', view_func=get_hiring_stages)
 
 def get_hiring_stages():
     response_object = {'status': 'success'}
