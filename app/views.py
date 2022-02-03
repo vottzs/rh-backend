cc0b9a01652d2f18ae4c73285d1d311817bde2b6
Angelo Ribeiro <angelo@vottz.com>
2022-02-02 18:49:21 -0800

    first frontend calls

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
+from unittest.mock import patch
 from flask import Flask, jsonify
+from flask.globals import request
 from app.db.mocked_data import DEFAULT_HIRING_STAGES, CANDIDATES, JOB_POSTINGS
 
 def add_views(app: Flask):
     app.add_url_rule('/api/v1/hiring_stages', view_func=get_hiring_stages)
     app.add_url_rule('/api/v1/candidates', view_func=get_candidates)
-    app.add_url_rule('/api/v1/candidates/<candidate_id>', view_func=get_candidate)
+    app.add_url_rule('/api/v1/candidates/<candidate_id>', view_func=get_candidate, methods=['GET', 'POST'])
     app.add_url_rule('/api/v1/job_postings', view_func=get_job_postings)
 
 
@@ -16,8 +18,16 @@ def get_hiring_stages():
 
 
 def get_candidates():
+    stage = request.args.get('stage')
     response_object = {'status': 'success'}
-    response_object['candidates'] = CANDIDATES
+    if stage is not None:
+        filtered_candidates = []
+        for candidate in CANDIDATES:
+            if candidate['stage'] == stage:
+                filtered_candidates.append(candidate)
+        response_object['candidates'] = filtered_candidates
+    else:
+        response_object['candidates'] = CANDIDATES
     response = jsonify(response_object)
     return response
 
@@ -31,7 +41,14 @@ def get_job_postings():
 
 def get_candidate(candidate_id):
     response_object = {'status': 'success'}
-    response_object['candidate'] = search_on_list(CANDIDATES, 'id', int(candidate_id))
+    if candidate_id is None:
+        return {'status': 'failed'}
+    candidate = search_on_list(CANDIDATES, 'id', int(candidate_id))
+    if request.method == 'POST':
+        patch_data = request.json
+        if 'stage' in patch_data:
+            candidate['stage'] = patch_data['stage']
+    response_object['candidate'] = candidate
     response = jsonify(response_object)
     return response
 
